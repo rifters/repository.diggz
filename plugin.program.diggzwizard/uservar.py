eebd24451bb1e0077dd9caa22a5e6753c320f8b1
Jason Diggz <jdig4240@users.noreply.github.com>
2017-07-08 12:39:28 -0400

    Updtate Wizard
    Update to 3.5

--- plugin.program.diggzwizard/uservar.py
+++ plugin.program.diggzwizard/uservar.py
@@ -5,7 +5,7 @@ import os, xbmc, xbmcaddon
 #########################################################
 ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
 ADDONTITLE     = 'Diggz Wizard'
-EXCLUDES       = [ADDON_ID, 'repository.diggz', 'plugin.program.diggzwizard']
+EXCLUDES       = [ADDON_ID]
 # Text File with build info in it.
 BUILDFILE      = 'http://wizardfiles.jdiggz.com/wizrdtexts/diggzwizard.txt'
 # How often you would list it to check for build updates in days
@@ -17,7 +17,7 @@ APKFILE        = 'http://wizardfiles.jdiggz.com/wizrdtexts/Diggzwizardapk.txt'
 YOUTUBETITLE   = ''
 YOUTUBEFILE    = 'http://'
 # Text File for addon installer.  Leave as 'http://' to ignore
-ADDONFILE      = 'http://wizardfiles.jdiggz.com/wizrdtexts/DiggzWizardaddons.txt'
+ADDONFILE      = 'http://'
 # Text File for advanced settings.  Leave as 'http://' to ignore
 ADVANCEDFILE   = 'http://'
 
@@ -36,14 +36,14 @@ ART            = os.path.join(PATH, 'resources', 'art')
 # Leave as http:// for default icon
 ICONBUILDS     = 'http://wizardfiles.jdiggz.com/DiggzWizardpngs/build-icon.png'
 ICONMAINT      = 'http://wizardfiles.jdiggz.com/DiggzWizardpngs/mantanence-icon.png'
-ICONAPK        = 'http://wizardfiles.jdiggz.com/DiggzWizardpngs/apk-icon.png'
+ICONAPK        = 'http://wizardfiles.jdiggz.com/DiggzWizardpngs/apkinstaller.png'
 ICONADDONS     = 'http://wizardfiles.jdiggz.com/DiggzWizardpngs/addoninstaller-1.png'
 ICONYOUTUBE    = 'http://wizardfiles.jdiggz.com/DiggzWizardpngs/youtube.png'
 ICONSAVE       = 'http://wizardfiles.jdiggz.com/DiggzWizardpngs/savedata.png'
 ICONTRAKT      = 'http://wizardfiles.jdiggz.com/DiggzWizardpngs/trakt.png'
 ICONREAL       = 'http://wizardfiles.jdiggz.com/DiggzWizardpngs/real-debrid-1.png'
 ICONLOGIN      = 'http://wizardfiles.jdiggz.com/DiggzWizardpngs/login.png'
-ICONCONTACT    = 'http://wizardfiles.jdiggz.com/DiggzWizardpngs/contact%20us.png'
+ICONCONTACT    = 'http://'
 ICONSETTINGS   = 'http://wizardfiles.jdiggz.com/DiggzWizardpngs/settingsicon.png'
 # Hide the ====== seperators 'Yes' or 'No'
 HIDESPACERS    = 'No'
@@ -52,8 +52,8 @@ SPACER         = '='
 
 # You can edit these however you want, just make sure that you have a %s in each of the
 # THEME's so it grabs the text from the menu item
-COLOR1         = 'slateblue'
-COLOR2         = 'slateblue'
+COLOR1         = 'indigo'
+COLOR2         = 'white'
 # Primary menu items   / %s is the menu item and is required
 THEME1         = '[COLOR '+COLOR1+'][B][I]([COLOR '+COLOR2+']Diggz[/COLOR])[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR][/I]'
 # Build Names          / %s is the menu item and is required
@@ -69,7 +69,7 @@ THEME5         = '[COLOR '+COLOR1+']Current Theme:[/COLOR] [COLOR '+COLOR2+']%s[
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
 HIDECONTACT    = 'No'
 # You can add \n to do line breaks
-CONTACT        = 'Thank you for choosing Aftermath Wizard.\r\n\r\nContact us on facebook at http://facebook.com'
+CONTACT        = 'Thank you for choosing Diggz Wizard.\r\n\r\n'
 #Images used for the contact window.  http:// for default icon and fanart
 CONTACTICON    = 'http://'
 CONTACTFANART  = 'http://'
