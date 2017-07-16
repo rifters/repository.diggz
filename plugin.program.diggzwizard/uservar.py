586067ab91a329f9a740b1fc5949a855396f4389
Jason Diggz <jdig4240@users.noreply.github.com>
2017-07-16 16:18:30 -0400

    3.6
    Wizard colors fixed

--- plugin.program.diggzwizard/uservar.py
+++ plugin.program.diggzwizard/uservar.py
@@ -52,7 +52,7 @@ SPACER         = '='
 
 # You can edit these however you want, just make sure that you have a %s in each of the
 # THEME's so it grabs the text from the menu item
-COLOR1         = 'indigo'
+COLOR1         = 'dodgerblue'
 COLOR2         = 'white'
 # Primary menu items   / %s is the menu item and is required
 THEME1         = '[COLOR '+COLOR1+'][B][I]([COLOR '+COLOR2+']Diggz[/COLOR])[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR][/I]'
